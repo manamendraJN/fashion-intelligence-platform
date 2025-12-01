36e0728057ad23ba9d6465e930478c98c5b4a7f9
Navodya <127787000+manamendraJN@users.noreply.github.com>
2025-12-01 23:57:04 +0530

    Feature/body measurement and sizing (#1)
    * create simple project folder structure
    
    * create react frontend
    
    * create components and services folders inside the frontend directory

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+  <h1 class="text-3xl font-bold text-center underline text-fuchsia-600">
+    Hello world!
+  </h1>
+    </>
+  );
+}
+
+export default App
